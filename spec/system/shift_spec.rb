ad2741367e29357c10b2f2d0a9e29afea2d7f264
kuboichi <kuboichi124@gmail.com>
2023-12-29 15:37:06 +0000

    Change the way to check save is successful

--- spec/system/shift_spec.rb
+++ spec/system/shift_spec.rb
@@ -13,17 +13,16 @@ RSpec.describe "shifts", type: :system do
     end
 
     describe "Shift.new" do
-
         it 'Create a new shift' do
             visit new_shift_path
-            click_button "Save"
-            expect(page).to have_content "保存成功"
+            expect {
+                click_button "Save"
+            }.to change(Shift, :count).by(1)
         end
 
         it "Create a wrong shift data" do
             visit new_shift_path
             fill_in "shift[week_day_0_0]", with: nil
-            week_day_0_0= nil
             click_button "Save"
             expect(page).to have_content "保存失敗"
         end
