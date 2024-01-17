1e8fe6a76d3bcba31f365bd33e81ae653e082dc9
kuboichi <kuboichi124@gmail.com>
2024-01-17 12:10:07 +0900

    Change settings for deploy

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  root "static_pages#home"
-  get 'static_pages/home'
-  get 'static_pages/help'
+  root "shifts#new"
+  get 'static_pages/home', to: 'static_pages#home', as: :home
+  get 'static_pages/help', to: 'static_pages#help', as: :help
   get 'shifts/share', to: 'shifts#share', as: :share
   get 'shifts/show/:token', to: 'shifts#show', as: :show
   get 'shifts/determined/:token', to: 'shifts#determined', as: :determined
